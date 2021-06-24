67ec96079135a1de8a4a9cb6c9e52e8787db0a6b
AlexanderCarlston <alekscarlston@gmail.com>
2021-06-24 14:52:59 -0600

    Add solution

---
+++ problems/parenthesis/first-implementation.js
@@ -0,0 +1,36 @@
+function isValid(s) {
+  if (s.length < 2 ) { return false }
+  let pStack = []
+  const bracketPairs = {
+    '{': '}',
+    '(': ')',
+    '[': ']'
+}
+
+  for (let i = 0; i < s.length; i++) {
+    let currentSubString = s.charAt(i)
+
+    if (isOpenBrace(currentSubString)){
+      pStack.push(currentSubString)
+    } else {
+      if(!isValidClosingBrace(pStack.pop(), currentSubString)) {
+        return false
+      }
+    }
+  }
+
+  if (pStack.length > 0) {
+    return false
+  }
+
+  return true
+};
+
+function isOpenBrace(brace) {
+  const validOpenBraces = Object.keys(bracketPairs)
+  return validOpenBraces.includes(brace)
+}
+
+function isValidClosingBrace(openBrace, closeBrace) {
+  return bracketPairs[openBrace] === closeBrace;
+}
